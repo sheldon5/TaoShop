866570912d71086eb468c1834b41098c822c452f
sheldon5 <sheldonisready@gmail.com>
2017-09-06 20:35:09 +0900

    TaoShop FirstCommit

---
+++ taotao-manager-web/src/main/java/com/taotao/controller/PageController.java
@@ -0,0 +1,28 @@
+package com.taotao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 
+ * @author 6waves
+ *
+ */
+@Controller
+public class PageController {
+	
+	@RequestMapping("/")
+	public String showIndex(){
+		return "index";
+	}
+	
+	
+	@RequestMapping("/{page}")
+	public String showPage(@PathVariable String page){
+		return page;
+	}
+	
+	
+
+}
